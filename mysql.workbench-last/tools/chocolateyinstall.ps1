fd6663b90d29ecb6dbcaa3255cd688536c560410
Marcos André de Almeida <marcos@xqx.com.br>
2018-02-17 17:12:18 -0200

    Add files via upload

---
+++ mysql.workbench-last/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'mysql.workbench-last'
+$url = 'http://xqx.com.br/public/mysql-workbench-community-6.3.10-winx64.msi'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  silentArgs    = "/passive /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  url           = $url
+}
+
+Install-ChocolateyPackage @packageArgs
